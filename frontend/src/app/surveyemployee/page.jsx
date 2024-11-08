799968ab8e40fd99de1f690e396c1d42b7b1498a
temevh <temehama@gmail.com>
2024-11-08 23:44:30 +0200

    continuing questionnaire

--- frontend/src/app/surveyemployee/page.jsx
+++ frontend/src/app/surveyemployee/page.jsx
@@ -61,6 +61,7 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <p className="text-3xl text-white text-bold">EMPLOYEE FOR EMRE OY</p>
       <p className="text-3xl text-white">
         {questions[currentQuestionIndex].question}
       </p>
