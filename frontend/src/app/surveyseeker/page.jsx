72483c63f531501954183cd8881133f33633a23f
temevh <temehama@gmail.com>
2024-11-09 14:58:37 +0200

    fix

--- frontend/src/app/surveyseeker/page.jsx
+++ frontend/src/app/surveyseeker/page.jsx
@@ -52,7 +52,7 @@ export default function Home() {
     );
   }
 
-  if (currentQuestionIndex >= questions.length) {
+  if (currentQuestionIndex == 8) {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <Button variant="contained" color="secondary" onClick={submitPress}>
