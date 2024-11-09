7091da1db7960c7b9a992d54433552b117c6f6c9
temevh <temehama@gmail.com>
2024-11-09 12:35:28 +0200

    fixed question index problem

--- frontend/src/app/surveyemployee/page.jsx
+++ frontend/src/app/surveyemployee/page.jsx
@@ -67,7 +67,7 @@ export default function Home() {
     );
   }
 
-  if (currentQuestionIndex == 7) {
+  if (currentQuestionIndex == 8) {
     console.log("Finished");
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
