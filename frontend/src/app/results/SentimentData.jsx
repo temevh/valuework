02a3ed87fbad1ae5442985fa45c7fce17f8a686b
temevh <temehama@gmail.com>
2024-11-09 22:56:49 +0200

    added AI info popup

--- frontend/src/app/results/SentimentData.jsx
+++ frontend/src/app/results/SentimentData.jsx
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const sentimentData = {
   _id: {
@@ -20,18 +22,34 @@ const sentimentData = {
 
 const SentimentData = () => {
   const data = sentimentData;
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="bg-gray-100 p-4 rounded-lg mb-4 shadow-inner">
-      <h3 className="text-xl font-bold mb-2 text-black">Company Trends</h3>
+      <div className="flex flex-row">
+        <h3 className="text-xl font-bold mb-2 text-black">Company Trends</h3>
+        <FontAwesomeIcon
+          icon={faInfoCircle}
+          className="text-gray-500 ml-2"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        />
+        {isHovered && (
+          <div className="absolute top-0 w-48 left-100 mt-6 ml-8 p-2 bg-white border border-gray-300 rounded shadow-lg">
+            <p className="text-gray-700 text-sm">
+              The trend overview is based on news analysed by AI and ML models
+            </p>
+          </div>
+        )}
+      </div>
       {data.label.map((label, index) => (
         <div key={index} className="flex items-center justify-between">
           <p className="text-black text-md">{label}</p>
           <p className="text-black text-md">
             {data.sentiment[index] === 1 ? (
-              <span className="text-green-500">&#9650;</span> // Green up arrow
+              <span className="text-green-500">&#9650;</span>
             ) : (
-              <span className="text-red-500">&#9660;</span> // Red down arrow
+              <span className="text-red-500">&#9660;</span>
             )}
           </p>
         </div>
